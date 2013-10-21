4269957a76698f180c3af36130005c2a3d02ffc4
Christobot <dxdstudio@gmail.com>
2013-10-21 10:56:02 -0700

    Add first day's project.

---
+++ 10-21-2013/swing.js
@@ -0,0 +1,39 @@
+;(function(){
+    function rnd(num) {
+        return Math.round(Math.random() * num);
+    }
+    function rand() {
+        return rnd(255) + ', ' + rnd(255) + ', ' + rnd(255);
+    }
+
+    var canvas = new fabric.Canvas('my-canvas');
+
+    // update h/w
+    $('div, canvas').css({
+        'width': $(window).width(),
+        'height': $(window).height()
+    });
+
+    // add elements
+    for(var i = 0; i <= 20; i++) {
+        canvas.add(new fabric.Rect({
+            fill: 'red',
+            width: i,
+            height: i,
+            angle: i,
+            left: i * i,
+            top: i * i
+        }));
+    }
+
+    // adjust on mousemove
+    canvas.on('mouse:move', function(data){
+        var objects = canvas.getObjects();
+        $(objects).each(function(k, object){
+            object.height = data.e.layerX;
+            object.width = data.e.layerY;
+            object.fill = 'rgb(' + rand() + ')';
+            canvas.renderAll();
+        });
+    });
+})();
