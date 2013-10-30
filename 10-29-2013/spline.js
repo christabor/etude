8cf71a73d3be3d75ea85a2ec5414c93110406fb4
christaborium <digital@dxdstudio.com>
2013-10-30 00:18:38 -0700

    Add new day - plugin

--- 10-29-2013/spline.js
+++ 10-29-2013/spline.js
@@ -1,19 +1,45 @@
 $(document).ready(function(){
-    var phrase = $('.loading-phrases'),
-    phrases = phrase.find('li'),
-    count = phrases.length,
-    advance,
-    current = 0;
+    $.fn.spline = function(options){
+        var phrase = $('.spline-loader'),
+        defaults = {
+            delay: 1000,
+            transition: 1000,
+            loop: true
+        },
+        opts = $.extend(defaults, options),
+        phrases = phrase.find('li'),
+        count = phrases.length,
+        advance,
+        current = 0;
 
-    phrases.hide();
-    advance = setInterval(function(){
-        phrases.eq(current).show();
-        phrases.eq(current).prevAll().hide();
-        current += 1;
-    }, 1000);
+        phrase.css({
+            'padding': '0',
+            'margin': '0 0 0 -50%',
+            'left': '50%',
+            'height': parseInt(phrase.css('font-size').replace('px', ''), 10) + 10 + 'px',
+            'width': '100%',
+            'position': 'relative'
+        });
+        phrases.hide().css({
+            'list-style-type': 'none',
+            'width': '100%',
+            'position': 'absolute'
+        });
+        advance = setInterval(function(){
+            phrases.eq(current).fadeIn(opts.transition);
+            phrases.eq(current).prevAll().fadeOut(opts.transition);
+            current += 1;
+        }, opts.delay);
 
-    if(count === current) {
-        clearInterval(advance);
-    }
-    return;
+        if(count === current) {
+            clearInterval(advance);
+        }
+        return;
+    };
+
+    // init
+    $('body').spline({
+        transition: 100,
+        delay: 2000
+    });
 });
