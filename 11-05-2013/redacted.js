17e1444bdeb836eb768d34fe1c3b825b9d55f7c2
Christobot <dxdstudio@gmail.com>
2013-11-06 12:32:56 -0800

    Rewrite entirely for optimization purposes.

--- 11-05-2013/redacted.js
+++ 11-05-2013/redacted.js
@@ -1,34 +1,50 @@
 $(document).ready(function(){
-	function redact() {
-		$('.message').find('p').each(function(){
-			// split each paragraph by newline
-			var text = $(this).text().split('\n'),
-			redacted = '';
-			$(text).each(function(k, line){
+    var message = $('#message'),
+    been_redacted = false;
 
-				// loop through each line, split newlines by spaces
-				// to create blocks of text
-				var blocks = line.split(' ');
-				$(blocks).each(function(k, block){
+    function reredact() {
+        log('re-redacting');
+        // redact a second time, skip some unnecessary steps.
+        message.find('span').each(function(k, span){
+            if(Math.random() * 10 > 5) {
+                $(span).removeClass('redacted');
+            } else {
+                $(span).addClass('redacted');
+            }
+        });
+        return;
+    }
 
-					// randomly choose if redacted or not
-					if(Math.random() * 10 > 5) {
-						redacted += ' <span class="redacted">' + block + '</span> ';
-					}
-					else {
-						redacted += ( ' ' + block + ' ');
-					}
-				});
-			});
-
-			// update html
-			$(this).html(redacted);
-			return;
-		});
-		return;
-	}
-	$('#redactor').on('click', function(){
-		redact();
-		$(this).text('Redact it again.');
-	});
+    function redact() {
+        log('redacting');
+        message.find('p').each(function(){
+            // split each paragraph by newline
+            var redacted = '',
+            text = $(this).text().split('\n');
+            $(text).each(function(k, line){
+                var chunks = line.split(' ');
+                $(chunks).each(function(k, chunk){
+                    if(Math.random() * 10 > 5) {
+                        redacted += ' <span>' + $.trim(chunk) + '</span> ';
+                    } else {
+                        redacted += ' <span class="redacted">' + $.trim(chunk) + '</span> ';
+                    }
+                });
+            });
+            // update html
+            $(this).html(redacted);
+        });
+        message.find('span:empty').remove();
+        been_redacted = true;
+        return;
+    }
+    $('#redactor').on('click', function(){
+        // check if it's been done already
+        if(been_redacted) {
+            reredact();
+        } else {
+            redact();
+        }
+        $(this).text('Redact it again.');
+    });
 });
