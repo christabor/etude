4feb6651a65dafb308222261b655b4e48e95bf4a
christabor <dxdstudio@gmail.com>
2014-01-24 00:33:10 -0800

    New day, add

---
+++ 01-23-2014/ordered.js
@@ -0,0 +1,30 @@
+var dims        = getViewportDimensions();
+var height      = dims.height;
+var width       = dims.width;
+var container   = $('#container');
+
+
+function sequenceSet(base, count) {
+    var block = $('<div class="block"></div>');
+    var start = 0;
+    while(start < count) {
+        block.append('<span>' + (base * start) + '</span>');
+        start += 1;
+    }
+    container.append(block);
+}
+
+function init() {
+    var start = 1;
+    var count = 15;
+    var max = 100;
+    doSomethingABunch(function(){
+        var current;
+        sequenceSet(start, count);
+        current = container.find('.block').last();
+        current.prepend('<p>Start ' + start + '</p>');
+        start += 1;
+    }, max);
+}
+
+$(document).ready(init);
