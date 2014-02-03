beabbb549c951045ee0a7103564bc17c5e7d17c4
christabor <dxdstudio@gmail.com>
2014-02-03 01:50:26 -0800

    Add new day, remove unnecessary libraries

---
+++ 02-02-2014/starsy.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+    var dims        = getDocumentDimensions();
+    var height      = dims.height;
+    var width       = dims.width;
+    var canvas_elem = document.querySelector('canvas');
+    var formula     = [];
+    var total       = 50;
+    var current     = 0;
+    var fg = '';
+    var bg = '';
+
+    function addGroup(seed_x, seed_y) {
+        if(current === total) {
+            canvas.clear();
+            current = 0;
+            addText();
+
+            // swap colors
+            bg = '#213D5A';
+            fg = '#43B02A';
+        } else {
+            fg = '#213D5A';
+            bg = '#43B02A';
+        }
+        for(var i = 0; i <= 8; i++) {
+            var circ = new fabric.Circle({
+                radius: i * 5,
+                fill: fg,
+                selectable: false,
+                opacity: i * 0.5,
+                top: seed_y * i,
+                left: seed_x * i
+            });
+            canvas.add(circ);
+            circ.animate({
+                radius: rando(width / 4),
+                duration: 100,
+                onChange: canvas.renderAll.bind(canvas),
+                onComplete: function() {
+                    circ.animate({
+                        radius: rando(50),
+                        duration: 100,
+                        onChange: canvas.renderAll.bind(canvas)
+                    });
+                }
+            });
+        }
+        canvas.backgroundColor = bg;
+        current += 1;
+    }
+
+    function addRandomGroup() {
+        var x = rando(width);
+        var y = rando(height)
+        addGroup(x, y);
+        canvas.renderAll();
+    }
+    function addText() {
+        canvas.add(new fabric.Text('Go Seahawks!', {
+            top: height / 2 - 100,
+            left: width / 2,
+            fontSize: 100,
+            fill: 'white',
+            selectable: false,
+            fontFamily: 'Lato'
+        }));
+    }
+
+    function init() {
+        canvas_elem.width  = width;
+        canvas_elem.height = height;
+        canvas             = new fabric.Canvas('canvas');
+        canvas.selection   = false;
+        canvas.backgroundColor = bg;
+        addText();
+        setInterval(addRandomGroup, 100);
+    }
+    init();
+};
