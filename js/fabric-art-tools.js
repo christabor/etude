e158992a2f2f1d45f9d2f37b8096a716f54deae3
christabor <dxdstudio@gmail.com>
2014-02-23 01:08:37 -0800

    Add more art utils

--- js/fabric-art-tools.js
+++ js/fabric-art-tools.js
@@ -1,7 +1,7 @@
 /*
 
 Art based tools for use in Fabric.js
-Dependencies: utils.js
+Dependencies: utils.js, fabric.js
 
 */
 
@@ -117,4 +117,121 @@ function addSpirograph(width, height, times, fill, reverse) {
             angle += increment;
         }
     }, times);
+}
+
+function alternateRingsY(x, y) {
+    // use a sub processes to calculate
+    // new x, y values
+    // - used with -> addRings etc..
+    y += 100;
+    if(rando(10) > 4) {
+        x += 100;
+    } else {
+        x -= 100;
+    }
+    return [x, y];
+}
+
+function alternateRingsX(x, y) {
+    // use a sub processes to calculate
+    // new x, y values
+    // - used with -> addRings etc..
+    x += 100;
+    if(rando(10) > 4) {
+        y += 100;
+    } else {
+        y -= 100;
+    }
+    return [x, y];
+}
+
+function addSporadicUniformRings(opts) {
+    doSomethingABunch(function(){
+        var coords;
+        canvas.add(new fabric.Circle({
+            top: opts.y,
+            left: opts.x,
+            radius: opts.radius,
+            selectable: false,
+            opacity: 1,
+            fill: 'none',
+            strokeWidth: opts.thickness,
+            stroke: opts.color || randomColor(255)
+        }));
+        if(opts.direction === 'vertical') {
+            coords = alternateRingsY(opts.x, opts.y);
+        } else {
+            coords = alternateRingsX(opts.x, opts.y);
+        }
+        opts.x = coords[0];
+        opts.y = coords[1];
+    }, opts.max);
+}
+
+function addWideGrowingRings(opts) {
+    doSomethingABunch(function(){
+        var coords;
+        canvas.add(new fabric.Circle({
+            top: opts.y,
+            left: opts.x,
+            radius: opts.radius,
+            selectable: false,
+            opacity: 1,
+            fill: 'none',
+            strokeWidth: opts.thickness,
+            stroke: opts.color || randomColor(255)
+        }));
+        opts.radius += opts.radius * opts.magnitude;
+        if(opts.direction === 'vertical') {
+            coords = alternateRingsY(opts.x, opts.y);
+        } else {
+            coords = alternateRingsX(opts.x, opts.y);
+        }
+        x = coords[0];
+        y = coords[1];
+    }, opts.max);
+}
+
+function spiralRepeater(opts) {
+    // @opts.magnitude: controls the intensity
+    // of each angle rotation, from 0 - 1
+    // @opts.shape: Flexible enough to let you
+    // determine what kind of shape to use
+    // -- options: triangle, rect, path
+    // @opts.stretch: optional stretch to distort h/w
+    var angle = 0;
+    var amt   = Math.floor(360 / opts.max);
+    doSomethingABunch(function(){
+        var pathdata;
+        var path;
+        var args  = {
+            width: opts.size,
+            height: opts.size / (opts.stretch || 1),
+            opacity : 0.8,
+            stroke: opts.stroke || 'rgba(0,0,0,1)',
+            selectable: false,
+            strokeWidth: opts.thickness || 1,
+            angle: angle,
+            fill: randomColor(255),
+            top: opts.y,
+            left: opts.x
+        };
+        if(opts.shape === 'rect') {
+            canvas.add(new fabric.Rect(args));
+        } else if(opts.shape === 'triangle') {
+            canvas.add(new fabric.Triangle(args));
+        } else if(opts.shape === 'path') {
+            if(!opts.pathdata) {
+                pathdata = smartPath(opts.size / 12, opts.size / 12);
+            }
+            path = new fabric.Path(pathdata);
+            path.set(args);
+            canvas.add(path);
+        } else {
+            return;
+        }
+        angle += amt * opts.angle_magnitude;
+        opts.x += opts.size / 8;
+        opts.y += opts.size / 4;
+    }, opts.max);
 }
