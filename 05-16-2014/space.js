56b770f400cb5ef81984096ee0aa6cc83b8803dd
christobot <dxdstudio@gmail.com>
2014-05-17 17:01:41 -0700

    Fix bg stretch

--- 05-16-2014/space.js
+++ 05-16-2014/space.js
@@ -1,27 +1,13 @@
 var playdrawr = (function(){
     var dims        = getViewportDimensions();
     var canvas_elem = document.querySelector('canvas');
-    var height      = dims.height/1.4;
-    var width       = dims.width/2;
-    var $container  = $('#svgcontainer');
-
-    function animateSVG(data) {
-        $container.show();
-        $container.find('g').hide().on('click', function(e){
-            $(this).attr('fill', 'blue');
-            log(e);
-        });
-        $container.find('path').on('click', copy);
-        $container.find('#star').delay(100).fadeIn(100);
-    }
-
-    function copy(e) {
-        $(this).after($(this).clone().attr('fill', 'red'));
-    }
+    var height      = dims.height;
+    var width       = dims.width;
 
     function init() {
-        $container.hide();
-        $('#svgcontainer').load('star.svg', animateSVG);
+        var bg = document.getElementById('bg');
+        bg.setAttribute('width', width)
+        bg.setAttribute('height', height);
     }
 
     return {
