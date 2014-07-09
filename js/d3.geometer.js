9f01114c44739d8a63f3e7d2db85b05cccb7b511
christobot <dxdstudio@gmail.com>
2014-07-09 00:34:05 -0700

    Add new functions, finish drawNearAdjacentEdges

--- js/d3.geometer.js
+++ js/d3.geometer.js
@@ -5,22 +5,27 @@
 
 // Using semantic versioning. http://semver.org/
 var d3_geometer = {
-    'version': '0.1.2'
+    'version': '0.2.3'
 };
 
 d3_geometer.nGon = function(group) {
+    if(!d3) return console.error('d3 library not found :sadface:');
+
     // d3 style - chainable interfaces.
     var element      = null;
+    var GLOBAL_CLASS = 'd3_geometer';
+    var _connections = [];
     var tau          = Math.PI * 2;
     var STROKE       = '#b66d48';
     var FILL         = '#f4eae5';
     var STROKE_WIDTH = 3;
     var DOT_SIZE     = 5;
+    var RADIUS       = 6;
     var OFFSET       = 50;
-    var DASHARRAY    = 10;
+    var DASHARRAY    = 5;
     var line         = d3.svg.line()
-                       .x(function(d){return d.x;})
-                       .y(function(d){return d.y;});
+    .x(function(d){return d.x;})
+    .y(function(d){return d.y;});
 
     // Inner "parent" function that is **always** returned
     // in each helper function, which allows for chaining.
@@ -40,19 +45,24 @@ d3_geometer.nGon = function(group) {
         return nGon;
     }
 
+    nGon.destroy = function() {
+        element.remove();
+        group.selectAll('g').remove();
+    };
+
     nGon.getCoords = function(radius, sides) {
         // Generate the coordinates for
         // each point based on # of sides.
         return d3.range(sides).map(function(d){
-            var cx = 0;  // center x
-            var cy = 0;  // center y
-            var start_angle = 0;
+            var cx           = 0;  // center x
+            var cy           = 0;  // center y
+            var start_angle  = 0;
             var center_angle = tau / sides;
-            var curr_angle = start_angle + (d * center_angle);
-            var vx = Math.round(cx + radius * Math.cos(curr_angle));
-            var vy = Math.round(cy - radius * Math.sin(curr_angle));
+            var curr_angle   = start_angle + (d * center_angle);
+            var vx           = Math.round(cx + radius * Math.cos(curr_angle));
+            var vy           = Math.round(cy - radius * Math.sin(curr_angle));
             return {
-                'label': 'S' + d,
+                'label': 'L' + d,
                 'x': vx,
                 'y': vy
             };
@@ -62,43 +72,82 @@ d3_geometer.nGon = function(group) {
     nGon.drawLabels = function() {
         // Draw some labels on the vertices.
         group.append('g')
-        .classed({'ngon-labels': true})
+        .attr('class', [GLOBAL_CLASS, 'ngon-labels'].join(' '))
         .selectAll('.label')
         .data(element.datum())
         .enter()
         .append('text')
         .text(function(d){return d.label;})
-        .attr('x', function(d){return d.x - 4;})
-        .attr('y', function(d){return d.y - 10;});
+        .attr('x', function(d){return d.x - (RADIUS + RADIUS / 2);})
+        .attr('y', function(d){return d.y - (RADIUS + RADIUS / 2);});
         return nGon;
     };
 
     nGon.drawVertices = function() {
         // Draw vertices for each edge.
         group.append('g')
-        .classed({'ngon-vertices': true})
+        .attr('class', [GLOBAL_CLASS, 'ngon-vertices'].join(' '))
         .selectAll('.vertex')
         .data(element.datum())
         .enter()
         .append('circle')
-        .attr('r', 5)
+        .attr('r', 0)
         .attr('cx', function(d){return d.x})
-        .attr('cy', function(d){return d.y});
+        .attr('cy', function(d){return d.y})
+        .transition()
+        .delay(function(d, i){return i * 100;})
+        .attr('r', RADIUS);
         return nGon;
     };
 
-    nGon.drawConnectingEdges = function(edges) {
+    nGon.calculateEdgeOffsets = function(modulo) {
+        // Calculates all positions for each
+        // vertex to connect to - offset by one,
+        // since adjacent vertices are already connected
+        var datum = element.datum();
+        var len   = datum.length;
+        log(len);
+        if(len < 3) return;
+
+        function push(inner, j, i) {
+            inner.push({'x': datum[i].x, 'y': datum[i].y, 'label': 'C' + i});
+            inner.push({'x': datum[len - j].x, 'y': datum[len - j].y, 'label': 'C' + (len - j)});
+        }
+
+        // store connections state
+        _connections = d3.range(len).map(function(d, i){
+            var inner = [];
+            for(var j = 1; j < len - 1; j++) {
+                if(modulo) {
+                    if(j % modulo === 0) {
+                        push(inner, j, i);
+                    }
+                } else {
+                    push(inner, j, i);
+                }
+            }
+            return inner;
+        });
+        return nGon;
+    };
+
+    nGon.drawNearAdjacentEdges = function(connections) {
+        // Draws edges directly adjacent + 1 to each vertex.
+        // see calculateEdgeOffsets for details.
+        if(!connections) {
+            nGon.calculateEdgeOffsets();
+            connections = _connections;
+        }
         group.append('g')
-        .classed({'ngon-dash-edges': true})
+        .attr('class', [GLOBAL_CLASS, 'ngon-dash-edges'].join(' '))
         .selectAll('.dashed')
-        .data(edges || element.datum())
+        .data(connections)
         .enter()
         .append('path')
-        .attr('stroke-dasharray', DASHARRAY)
-        .attr('stroke-width', STROKE_WIDTH)
+        .attr('stroke-width', STROKE_WIDTH / 2)
+        .attr('stroke', STROKE)
         .attr('fill', 'none')
-        .attr('stroke', 'purple')
-        .attr('d', line)
+        .attr('d', line);
         return nGon;
     };
 
