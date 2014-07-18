42c392e66ba049beb035add3cb39e9c41eb9d300
christobot <dxdstudio@gmail.com>
2014-07-17 22:22:19 -0700

    Pull out widget code into modules, add new day and widget.

--- 07-16-2014/modality.js
+++ 07-16-2014/modality.js
@@ -1,54 +1,8 @@
-var modality = (function(options){
-
-    // traditional namespace
-    var defaults = {
-        'blur': true,
-        'transition_classes': 'animated fadeInUp'
-    };
-    var opts    = $.extend(defaults, options);
-    var z_index = 999;
-    var $modals = $('.modal-container');
-
-    function openModal(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        var $target = $(this).data('modal-target');
-
-        if(opts.blur) {
-            $modals.addClass('blur');
-            $($target).removeClass('blur');
-        }
-
-        $($target)
-        .addClass('active')
-        .css('z-index', z_index + 1)
-        .find('.modal-dialog')
-        .addClass(opts.transition_classes);
-
-        z_index += 1;
-    }
-
-    function closeModal(e) {
-        // coupled to structure, which is probably a good thing.
-        $(this).parent().parent().parent().removeClass('active');
-        if(opts.blur) {
-            $modals.removeClass('blur');
-        }
-    }
-
-    // jquery namespace
-    $.fn.modality = function() {
-        $('a[data-modal-target]').on('click', openModal);
-        $('.modal-header').find('[data-close]').on('click', closeModal);
-    }
-})();
-
 var modality = (function(){
     'use strict';
 
-
     function init() {
-        $.fn.modality();
+        $.fn.widg_modal();
     }
 
     return {
---
+++ js/widgets/side-panel.js
@@ -0,0 +1,20 @@
+var widg_panel = (function(options){
+
+    // traditional namespace
+    var defaults = {};
+    var opts    = $.extend(defaults, options);
+    var $panels = $('[data-sidebar-target]')
+
+    function togglePanel(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        var anim_class = $(this).data('anim');
+        if(!anim_class) anim_class = '';
+        $('.sidebar-panel').toggleClass('open ' + anim_class);
+    }
+
+    // jquery namespace
+    $.fn.widg_panel = function() {
+        $panels.on('click', togglePanel);
+    }
+})();
