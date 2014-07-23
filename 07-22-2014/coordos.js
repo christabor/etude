0a76be16d82e413c08dbed698612d918554f5a16
christobot <dxdstudio@gmail.com>
2014-07-22 21:35:20 -0700

    Add new day

---
+++ 07-22-2014/coordos.js
@@ -0,0 +1,44 @@
+var coordos = (function(){
+    'use strict';
+    var dims    = getViewportDimensions();
+    // visual effect, to produce more square shape
+    dims.width  = dims.width - 200;
+    function init() {
+        var container = getSVG('map', dims, 'body');
+        var group = container.append('g');
+        var ngon_group = container.append('g')
+        .attr('transform', getCenterTranslation(dims));
+
+        var map = d3_geometer.coordSpace(group, dims, 4);
+        var ngon = d3_geometer.nGon(ngon_group);
+
+        var ngon_group2 = group.append('g')
+        .attr('transform', 'translate(300, 200)');
+        var ngon2 = d3_geometer.nGon(ngon_group2);
+
+        var ngon_group3 = group.append('g')
+        .attr('transform', 'translate(' + dims.width / 4 + ', 500)');
+        var ngon3 = d3_geometer.nGon(ngon_group3);
+
+        ngon(200, 5)
+        .drawLabels()
+        .drawVertices()
+        .drawCenterPoint(dims.height, dims.width, true);
+
+        ngon2(100, 8)
+        .drawLabels()
+        .drawVertices()
+        .drawCenterPoint(dims.height, dims.width, true);
+
+        ngon3(120, 3)
+        .drawLabels()
+        .drawVertices()
+        .drawCenterPoint(dims.height, dims.width, true);
+    }
+
+    return {
+        'init': init
+    };
+})();
+
+window.onload = coordos.init;
