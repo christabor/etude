cba0eacbb46963a24ade8bd840dcd60b460d12fa
Chris Tabor <dxdstudio@gmail.com>
2015-11-05 00:13:05 -0800

    Add task

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,5 +20,6 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('watchIt', ['watch']);
     grunt.registerTask('default', ['sass']);
 };
